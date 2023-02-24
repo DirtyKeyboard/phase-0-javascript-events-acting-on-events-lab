54a8616ec097e93dfff3d2b66e7afad0e9e9e643
Andrew Hawileh <andrewh829@gmail.com>
2023-02-24 08:57:24 -0600

    done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+const speed = 5;
+const dodger = document.getElementById('dodger');
+dodger.style.backgroundColor = '#32FA01';
+dodger.style.bottom = '20px';
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'ArrowLeft')
+        moveDodgerLeft();
+    else if (e.key === 'ArrowRight')
+        moveDodgerRight();
+})
+function moveDodgerLeft()
+{
+    let m = parseInt(dodger.style.left.replace("px", ""))
+    console.log(m);
+    m -= speed;
+    dodger.style.left = m+"px";
+}
+
+function moveDodgerRight()
+{
+    let m = parseInt(dodger.style.left.replace("px", ""));
+    console.log(m)
+    m += speed;
+    console.log(m+"px");
+    dodger.style.left = m+"px";
+}
